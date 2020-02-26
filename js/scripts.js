b6bde7ffc3da37688c7e1fd6eed948464abe8a3e
cneidson <carrie.eidson@nyu.edu>
2020-02-26 11:01:19 -0500

    Second Push

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,15 @@
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiY25laWRzb24iLCJhIjoiY2s3MmhkcHVpMDJiNDNlcjk5MXc5ejdzeSJ9.m2VrsWCLMueCL2O1gumaQA';
 
-
 // we want to return to this point and zoom level after the user interacts
 // with the map, so store them in variables
-var initialCenterPoint = [-73.991780, 40.676]
-var initialZoom = 13
-
+var initialCenterPoint = [-73.977361, 40.764295]
+var initialZoom = 11
 
 // create an object to hold the initialization options for a mapboxGL map
 var initOptions = {
   container: 'map-container', // put the map in this container
-  style: 'mapbox://styles/mapbox/dark-v10', // use this basemap
+  style: 'mapbox://styles/mapbox/light-v10', // use this basemap
   center: initialCenterPoint, // initial view center
   zoom: initialZoom, // initial view zoom level (0-18)
 }
@@ -28,9 +26,9 @@ map.addControl(new mapboxgl.NavigationControl());
 map.on('style.load', function() {
 
   // add a geojson source to the map using our external geojson file
-  map.addSource('evacuation_routes', {
+  map.addSource('historic', {
     type: 'geojson',
-    data: './data/evacuation_routes.geojson',
+    data: './data/historic.geojson',
   });
 
   // let's make sure the source got added by logging the current map state to the console
@@ -38,65 +36,19 @@ map.on('style.load', function() {
 
   // add a layer for our custom source
   map.addLayer({
-    id: 'fill-evacuation_routes',
+    id: 'fill-historic',
     type: 'fill',
-    source: 'evacuation_routes',
+    source: 'historic',
     paint: {
-      'fill-color': {
-        type: 'categorical',
-        property: 'LandUse',
-        stops: [
-          [
-            '01',
-            LandUseLookup(1).color,
-          ],
-          [
-            '02',
-            LandUseLookup(2).color,
-          ],
-          [
-            '03',
-            LandUseLookup(3).color,
-          ],
-          [
-            '04',
-            LandUseLookup(4).color,
-          ],
-          [
-            '05',
-            LandUseLookup(5).color,
-          ],
-          [
-            '06',
-            LandUseLookup(6).color,
-          ],
-          [
-            '07',
-            LandUseLookup(7).color,
-          ],
-          [
-            '08',
-            LandUseLookup(8).color,
-          ],
-          [
-            '09',
-            LandUseLookup(9).color,
-          ],
-          [
-            '10',
-            LandUseLookup(10).color,
-          ],
-          [
-            '11',
-            LandUseLookup(11).color,
-          ],
+  'fill-color': '#ece2f0',
+  'fill-opacity': 0.8,
+  'fill-outline-color': 'black'
+}
+});
+});
 
-        ]
-      }
-    }
-  })
 
-  // add an empty data source, which we will use to highlight the lot the user is hovering over
+// add an empty data source, which we will use to highlight the lot the user is hovering over
   map.addSource('highlight-feature', {
     type: 'geojson',
     data: {
@@ -117,12 +69,34 @@ map.on('style.load', function() {
     }
   });
 
+
+  // add an empty data source, which we will use to highlight the lot the user is hovering over
+  map.addSource('highlight-feature', {
+    type: 'geojson',
+    data: {
+      type: 'FeatureCollection',
+      features: []
+    }
+  })
+
+  // add a layer for the highlighted lot
+  map.addLayer({
+    id: 'highlight-line',
+    type: 'line',
+    source: 'highlight-feature',
+    paint: {
+      'line-width': 3,
+      'line-opacity': 0.9,
+      'line-color': 'black',
+    }
+  });
+
   // listen for the mouse moving over the map and react when the cursor is over our data
 
   map.on('mousemove', function (e) {
     // query for the features under the mouse, but only in the lots layer
     var features = map.queryRenderedFeatures(e.point, {
-        layers: ['fill-pluto-bk-cd6'],
+        layers: ['fill-historic'],
     });
 
     // if the mouse pointer is over a feature on our layer of interest
@@ -132,9 +106,8 @@ map.on('style.load', function() {
 
       var hoveredFeature = features[0]
       var featureInfo = `
-        <h4>${hoveredFeature.properties.Address}</h4>
-        <p><strong>Land Use:</strong> ${LandUseLookup(parseInt(hoveredFeature.properties.LandUse)).description}</p>
-        <p><strong>Zoning:</strong> ${hoveredFeature.properties.ZoneDist1}</p>
+        <h4>${hoveredFeature.properties.area_name}</h4>
+        <p><strong>Description:</p>
       `
       $('#feature-info').html(featureInfo)
 
